b0f331376b9c401390d90935f0706cbc79bffb53
liangchangyou <liangchangyoujackson@gmail.com>
2020-05-16 18:24:56 +0800

    init

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module Gone
+module github.com/jacksonliang/Gone
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require github.com/gin-gonic/gin v1.6.3 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
